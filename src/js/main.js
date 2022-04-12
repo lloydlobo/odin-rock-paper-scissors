12d5617a216fd5e4c63e3e4556670fbe42ff2c9c
Lloyd Lobo <lloyd@lloydlobo.com>
2022-04-12 15:43:44 +0530

    WIP: Add helper functions inside playRound()
    For the scope of making a MVP workable game, the primary playRound()
    function is hereby extended.
    
    1. Add 3 functions to update the score when tied, won, lost.
    2. Bug with incrementing the score after each round.
    
    TODO:
    1. Add a function to update the score after each round.

--- src/js/main.js
+++ src/js/main.js
@@ -1,66 +1,84 @@
 // function to generate a random selection of the gameArray
 function computerPlay() {
   const gameArray = ["Rock", "Paper", "Scissors"];
-
   // random number generator to select the index of the gameArray
   const randomGameSelection =
     gameArray[Math.floor(Math.random() * gameArray.length)];
-  randomGameSelection;
 
   return randomGameSelection;
 }
-
-// hoisted function declaration
-computerPlay(); /* can call function above function definition */
+computerPlay();
 
 // function that plays a single round of the game
 function playRound(playerSelection, computerSelection) {
   let computerRandomSelection = computerPlay();
-  computerRandomSelection;
 
-  // make computerRandomSelection to lowercase
-  computerSelection = computerRandomSelection.toLowerCase();
-  computerSelection;
-
-  // playerSelection prompts the user to enter a selection
-  let playerSelectionPrompt = prompt("Rock, Paper, or Scissors?");
-  playerSelectionPrompt;
+  // prompt the user to type a choice
+  const playerSelectionPrompt = prompt("Rock, Paper, or Scissors?");
 
   // alert the user about their selections
   console.log(`You chose ${playerSelectionPrompt}.`);
-  console.log(
-    `Computer chose ${computerSelection[0].toUpperCase()}${computerSelection.slice(
-      1
-    )}.` /* convert the first string letter to uppercase */
-  );
+  console.log(`Computer chose ${computerRandomSelection}.`);
 
-  // make playerSelection to lower case
+  // convert player and computer Selection to lower case
   playerSelection = playerSelectionPrompt.toLowerCase();
+  computerSelection = computerRandomSelection.toLowerCase();
 
   // * playRound() result
   const playRoundResult = `${playerSelection}, ${computerSelection}`; /* string generated at each round */
 
-  // it then returns a string to declare a winner of the round
+  // statements to determine the round result
   const statementSuccess = `You had a better hand than the computer!\n${playerSelection} beats ${computerSelection}!`;
   const statementFailure = `The computer had a better hand!\n${computerSelection} beats ${playerSelection}!`;
   const statementTie = `It's a tie!\n${playerSelection} ties ${computerSelection}!`;
 
-  // declare game rules
+  // declare game rules /* [  ] can convert these 3 lines to helper function() */
   const rockBeatsScissors = "rock, scissors";
   const paperBeatsRock = "paper, rock";
   const scissorsBeatsPaper = "scissors, paper";
 
+  // declare Score
+  let playerScore = 0;
+  let computerScore = 0;
+
+  // runs when the game ties
+  function gameTies() {
+    playerScore;
+    computerScore;
+    console.log(`${statementTie}`);
+    console.log(`Player Score: ${playerScore}`);
+    console.log(`Computer Score: ${computerScore}`);
+  }
+
+  // runs when the player wins
+  function playerWins() {
+    playerScore++;
+    computerScore;
+    console.log(`${statementSuccess}`);
+    console.log(`Player Score: ${playerScore}`);
+    console.log(`Computer Score: ${computerScore}`);
+  }
+
+  // runs when the computer wins
+  function computerWins() {
+    computerScore++;
+    playerScore;
+    console.log(`${statementFailure}`);
+    console.log(`Player Score: ${playerScore}`);
+    console.log(`Computer Score: ${computerScore}`);
+  }
+
   // add game logic here...
   if (playerSelection === computerSelection) {
-    console.log(`${statementTie}`);
+    return gameTies();
   } else if (
     playRoundResult === rockBeatsScissors ||
     playRoundResult === paperBeatsRock ||
     playRoundResult === scissorsBeatsPaper
   ) {
-    console.log(`${statementSuccess}`);
+    return playerWins();
   } else {
-    console.log(`${statementFailure}`);
+    return computerWins();
   }
 
   // return results of each round
