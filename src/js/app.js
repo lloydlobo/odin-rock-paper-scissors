c346dd67e584478d7f278b94ded6e2f92cd5eb3d
Lloyd Lobo <lloyd@lloydlobo.com>
2022-04-18 18:58:50 +0530

    feat: Add event listener for each btn choice

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,16 @@
 console.dir(document.body);
 
+// shortcut for console.log
+let clg = (variable) => {
+  console.log(variable);
+};
+
 // get DOM elements by id
 const computerChoiceDisplay = document.getElementById("computerChoiceDisplay");
 const userChoiceDisplay = document.getElementById("userChoiceDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
+// console.log(resultDisplay); /* # => span#resultDisplay */
+// clg(resultDisplay); /* # => span#resultDisplay */
 
 // select all buttons with className of buttons
 const btnPossibleChoices = document.querySelectorAll(".buttonChoice");
@@ -16,9 +23,53 @@ let userChoice;
 // grab the buttons and for each possible choice
 btnPossibleChoices.forEach((btnPossibleChoice) =>
   btnPossibleChoice.addEventListener("click", (e) => {
-    userChoice = e.target.id;
+    userChoice = e.target.id; /* don't need to declare it again -> see l21 */
+    console.log(userChoice); /* # => btnScissors */ /* clicked scissors btn */
     // get userChoicesDisplay
   })
 );
 
-console.log(userChoice.id);
+// console.log(userChoice.id);
+
+// listen to key strokes 'a' 's' 'd'
+// a = inline-start(rock); b = center(paper); d = inline-end(scissors
+// let userKeyStrokes;
+
+// Grouping the elements of an array
+/*
+  Array.prototype.groupBy()
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+*/
+
+// let keys = [a,s,d];
+// keys.value.append = {rock, paper, scissors}
+// const choices = [
+//   {
+//     name: rock,
+//     image: "✊",
+//     key: "a",
+//     value: "rock",
+//     type: "traditional",
+//   },
+//   {
+//     name: paper,
+//     image: "✋",
+//     key: "s",
+//     value: "paper",
+//     type: "traditional",
+//   },
+//   {
+//     name: scissors,
+//     image: "🤞",
+//     key: "d",
+//     value: "scissors",
+//     type: "traditional",
+//   },
+// ];
+
+// let choicesResult = choices.groupBy(({ type }) => type);
+// console.log(choicesResult.traditional);
+
+// console.log(`These are the choices:\n${choicesArray}`);
+// forEach userKeyStrokes([a,s,d])
+// userKeyStrokes.addEventListener("[")
